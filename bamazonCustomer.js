247e877afa1ed57bd5af00febd60eb67b5a4f328
alongkot <nathankc94@gmail.com>
2019-05-11 09:21:06 -0500

    add inquirer prompt

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,22 +1,44 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-const {printTable} = require('console-table-printer');
-
+const { printTable } = require('console-table-printer');
+// connect to mySql
 const connection = mysql.createConnection({
-    host:"localhost",
+    host: "localhost",
     port: 3306,
-    user:"root",
+    user: "root",
     password: "password",
     database: "bamazon"
 })
-connection.connect(function(err){
+// check for errors and execute functions
+connection.connect(function (err) {
     if (err) throw err;
     displayTable();
 })
-
-function displayTable(){
-    connection.query("SELECT * FROM bamazon.products", function (err, res){
-    printTable(res);    
+// request data to display
+function displayTable() {
+    connection.query("SELECT * FROM bamazon.products", function (err, res) {
+        printTable(res);
+        selectId();
     })
-    
+
+}
+
+function selectId() {
+    inquirer.prompt([
+        {
+            name: "id",
+            type: "input",
+            message: "Enter the ID of the product you would like to purchase?"
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "How many would you like to purchase?"
+        }
+    ])
+        .then(answers => {
+            var Quantity = answers.quantity;
+            var ID = answers.id;
+
+        });
 }
