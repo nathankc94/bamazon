04b5e8d18e2b957bd39dc51edf2f3ac805a9a970
alongkot <nathankc94@gmail.com>
2019-05-14 13:28:16 -0500

    adding function to update quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const { printTable } = require('console-table-printer');
+
 // connect to mySql
 const connection = mysql.createConnection({
     host: "localhost",
@@ -17,33 +18,48 @@ connection.connect(function (err) {
 // request data to display
 function displayTable() {
     connection.query("SELECT * FROM bamazon.products", function (err, res) {
+        console.log("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
         printTable(res);
+        console.log("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
         selectId();
     })
 
-}
+};
 // ask clients for purchase id and quantity
 function selectId() {
     inquirer.prompt([
         {
             name: "id",
             type: "input",
-            message: "Enter the ID of the product you would like to purchase?"
+            message: "Enter the ID of the product you would like to purchase?",
+            filter: Number
         },
         {
             name: "quantity",
             type: "input",
-            message: "How many would you like to purchase?"
+            message: "How many would you like to purchase?",
+            filter: Number
         }
     ])
         // grab the answers(value)
         .then(answers => {
-            quantityValue = answers.quantity;
-            idValue = answers.id;
-            console.log(idValue);
-            console.log(quantityValue);
+            var idValue = answers.id;
+            var quantityValue = answers.quantity;
+            purchaseOrder(idValue, quantityValue)
         });
-    }
-    // 
-    var quantityValue;
-    var idValue;
+};
+// updating product quantity 
+function purchaseOrder(ID, QUANTITY){
+	connection.query('Select * FROM bamazon.products WHERE item_id = ' + ID, function(err,res){
+        if(err) console.log(err);
+		if(QUANTITY <= res[0].stock_quantity){
+            var totalCost = res[0].price * QUANTITY;
+            console.log("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
+            console.log("Your total cost for " + QUANTITY + " " +res[0].product_name + " is $" + totalCost + " Thank you!");
+			connection.query("UPDATE products SET stock_quantity = stock_quantity -  "+ QUANTITY +" WHERE item_id =" + ID + ";");
+		} else{
+			console.log("Insufficient quantity");
+		};
+		displayTable();
+	});
+};
